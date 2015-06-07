0f3621b25bd29c11e672a4fa9082899ac195905a
Jimi <jimi@jimis.net>
2015-06-07 07:33:12 +0300

    oxoxox

--- server.js
+++ server.js
@@ -5,6 +5,57 @@ var app = express();
 app.use(bodyParser());
 app.use(express.query());
 
+
+var Database = require('somewhere');
+var db = new Database('./db/db.json');
+var user = {
+  email: 'jimi@jimis.net',
+  password: 'test'
+}
+
+var User = function(data){
+  this.email = data.email || '';
+  this.password = data.password || '';
+  this.registered = Date.now();
+}
+
+var Dog = function(data){
+  this.name = data.name || '';
+  this.dob = data.dob || new Date();
+  this.weight = data.weight || 0;
+  this.height = data.height || 0;
+  this.gender = data.gender|| '';
+  this.breed = data.breed || '';
+  this.owner = data.owner || ''
+  this.imagelink = data.imagelink || '';
+  this.chipId = data.chipId || '';
+}
+
+var DogEvent = function(data){
+  this.eventType = data.eventType || '';
+  this.date = data.date || new Date();
+  this.description = data.description || '';
+  this.dog = data.dog || '';
+}
+var Symptom = function(data) {
+  this.name = data.name || '';
+  this.description = data.description || ''
+}
+
+var SymptomEvent = function(data){
+  this.startDate = data.startDate || new Date();
+  this.endDate = data.endDate || new Date();
+  this.symptom = data.symptom || '';
+  this.dog = data.dog || '';
+}
+
+var Disease = function(data) {
+  this.name = data.name || '';
+  this.symptoms = data.symptoms || [];
+}
+
+
+
 // mongoose.connect("mongodb://localhost/resources");
 // var DogSchema = mongoose.Schema({
 //   name: String,
